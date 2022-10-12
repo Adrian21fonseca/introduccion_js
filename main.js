875766784cea77162ed264207c1f8df61c69be1c
Adrian Fonseca <adrian2020fm@gmail.com>
2022-10-12 22:10:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+//DatosPersonales
+var Adrian21Fonseca =[
+    Informacion ={
+        nombre: "Adrian",
+        Apellido: "Fonseca",
+        edad: 20,
+        developer: false
+    },
+    cumpleaños ={
+        dia: 21,
+        mes: "agosto",
+        año: 2002
+    },
+    libroFav ={
+        titulo: "los juegos del hambre",
+        autor: "Suzanne Collins",
+        fecha: 14/9/2008,
+        referencia: "https://es.wikipedia.org/wiki/Los_juegos_del_hambre#:~:text=Los%20juegos%20del%20hambre%20se%20desarrolla%20en%20un,bajo%20su%20control%2C%20los%20cuales%20son%20llamados%20distritos."
+    },
+    pasatiempos ={
+        futbol, peliculas, cocinar
+    },
+    function miActividadFavorita(){
+        return "ver peluculas"
+    }
+]
